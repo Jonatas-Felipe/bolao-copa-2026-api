03ecfc31dc673717cec2929198dd3d8df2a509c5
Jonatas Felipe <jonatas-nandes@hotmail.com>
2026-06-13 12:45:19 -0300

    Ajustes

--- src/modules/users/services/LoginUserService.ts
+++ src/modules/users/services/LoginUserService.ts
@@ -1,5 +1,7 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+import { Raw } from "typeorm";
+
 import { AppDataSource } from '@shared/infra/typeorm/data-source.js'
 import { User } from '@modules/users/infra/typeorm/entities/User.js'
 import { Token } from '@modules/tokens/infra/typeorm/entities/Token.js'
@@ -21,7 +23,7 @@ export class LoginUserService {
     const userRepo = AppDataSource.getRepository(User)
     const tokenRepo = AppDataSource.getRepository(Token)
 
-    const user = await userRepo.findOneBy({ name })
+    const user = await userRepo.findOneBy({ name: Raw((alias) => `LOWER(${alias}) = LOWER(:value)`, { value: name }) })
     if (!user) {
       throw new AppError('Credenciais inválidas', 401)
     }
